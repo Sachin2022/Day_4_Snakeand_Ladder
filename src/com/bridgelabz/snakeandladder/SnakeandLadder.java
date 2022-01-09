e8139b1fc19c6818c3d04c9d3b47f25b1ddf018d
Textspot <sachinkatarki1995@gmail.com>
2022-01-09 21:38:05 +0530

    Added:Welcome to the Snake And Ladder Programme

---
+++ src/com/bridgelabz/snakeandladder/SnakeandLadder.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.snakeandladder;
+
+public class SnakeandLadder {
+	final static int NO_OF_PLAYER = 1;
+
+	public static void main(String[] args) {
+
+		System.out.println("Welcome To Snake And Ladder Game");
+		int position = 0;
+		System.out.println("Player start position : " + position);
+		System.out.println("Number of player is:" + NO_OF_PLAYER);
+	}
+
+}
